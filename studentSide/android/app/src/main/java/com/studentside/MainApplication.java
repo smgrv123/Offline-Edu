cc96a200e7c86eea24250f4ffdb75ff0e08d3e9e
shubhraagarwal <agarwalshubhra2002@gmail.com>
2021-07-11 03:12:21 +0530

    :rocket: Student Side app complete

--- studentSide/android/app/src/main/java/com/studentside/MainApplication.java
+++ studentSide/android/app/src/main/java/com/studentside/MainApplication.java
@@ -6,6 +6,7 @@ import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.masteratul.downloadmanager.ReactNativeDownloadManagerPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
--- studentSide/src/ConvertedImage/index.js
+++ studentSide/src/ConvertedImage/index.js
@@ -1,27 +1,12 @@
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {View, Button, Text} from './styles.js';
+import {View} from './styles.js';
 import {Image} from 'react-native';
 // import downloadManager from 'react-native-simple-download-manager';
 
 export default function ConvertedImage({route}) {
   const {text} = route.params;
-
-  // const url =
-  //   'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.9ZvAvUtuLbwQyGQxnKmxOgHaHa%26pid%3DApi&f=1';
-  // const headers = {Authorization: 'Bearer abcsdsjkdjskjdkskjd'};
-  // const config = {
-  //   downloadTitle: 'Title that should appear in Native Download manager',
-  //   downloadDescription:
-  //     'Description that should appear in Native Download manager',
-  //   saveAsName: 'File name to save',
-  //   allowedInRoaming: true,
-  //   allowedInMetered: true,
-  //   showInDownloads: true,
-  //   external: false, //when false basically means use the default Download path (version ^1.3)
-  //   path: 'Download/', //if "external" is true then use this path (version ^1.3)
-  // };
 
   return (
     <View>
@@ -31,20 +16,6 @@ export default function ConvertedImage({route}) {
         }}
         style={{height: 200, width: 150, alignSelf: 'center'}}
       />
-      {/* <Button
-        title="Download"
-        onPress={() =>
-          downloadManager
-            .download(url, headers, config)
-            .then(response => {
-              console.log('Download success!', response);
-            })
-            .catch(err => {
-              console.log('Download failed!', err);
-            })
-        }>
-        <Text>Download Image</Text>
-      </Button> */}
     </View>
   );
 }
